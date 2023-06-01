a725d06ff98068b2920ef5db1a6efaaa4da601ef
Archie <archie@mettro.com.au>
2023-06-01 23:42:20 +1000

    Updated env file to include site url

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
       }
     }
     try{
-      const response = await fetch('http://localhost:8000/completions', options)
+      const response = await fetch(process.env.SITE_URL + '/completions', options)
       const data = await response.json()
       setMessage(data.choices[0].message)
     } catch (error) {
