138262e4ed7a4d9f26edaf7418b9ba0277382c0f
Archie <archie@mettro.com.au>
2023-06-01 21:29:26 +1000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import { useState, useEffect } from 'react'
+
+
+const App = () => {
+  const [ value, setValue ] = useState(null)
+  const [ message, setMessage] = useState(null)
+  const [ previousChats, setPreviousChats ] = useState([])
+  const [ currentTitle, setCurrentTitle ] = useState(null)
+  const createNewChat = () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+    setMessage(null)
+    setValue("")
+  }
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try{
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   fetch('http://localhost:8000', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({message}),
+  //   })
+  //   .then((res) => res.json())
+  //   .then((data) => setResponse(data.message))
+  // }
+
+
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if (!currentTitle && value && message){
+      setCurrentTitle(value)
+    }
+    if(currentTitle && value && message){
+      setPreviousChats(prevChats => (
+        [...prevChats,
+          {
+            title: currentTitle,
+            role: "user",
+            content: value
+          },
+          {
+            title: currentTitle,
+            role: message.role,
+            content: message.content
+          }
+        ]
+      ))
+    }
+  }, [message, currentTitle])
+
+  console.log(previousChats)
+
+  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
+ 
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
+
+  //console.log(uniqueTitles)
+
+  return (
+    <div className="app">
+      <section className="side-bar">
+          <button onClick={createNewChat}>+ New chat</button>
+          <ul className="history">
+            {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+          </ul>
+          <nav>
+            Products
+          </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h2>Carpet One ChatGPT</h2>}
+        <ul className="feed">
+          {currentChat.map((chatMessage, index) => <li key={index}>
+            <p className="role">{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+           </li>)}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+              <div></div>
+
+              <input value={value} onChange={(e) => setValue(e.target.value)} />
+              <div id="submit" onClick={getMessages}>Send</div>
+
+              {/* <form onSubmit={handleSubmit}>
+                <textarea value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                <button type="submit">Submit</button>
+              </form> */}
+          </div>
+          <p className="info">
+          Carla, our virtual assistant, is a work in progress, but she is always learning and improving. We appreciate your patience as Carla continues to learn and grow.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App
